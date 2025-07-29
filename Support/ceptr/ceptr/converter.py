3846f33e9b679e11fb9e580a858b229cfadd2a1f
Thomas Howarth <69794367+ThomasHowarth@users.noreply.github.com>
2025-07-29 11:34:43 +0200

    Revert "#define num_lite_species"

--- Support/ceptr/ceptr/converter.py
+++ Support/ceptr/ceptr/converter.py
@@ -763,7 +763,6 @@ class Converter:
             cw.writer(
                 fstream,
                 f"#define {s}_ID {self.species_info.ordered_idx_map[species]}",
-
             )
             if s[-1] == "n" or s[-1] == "p" or s == "E":
                 nb_ions += 1
@@ -822,11 +821,6 @@ class Converter:
         cw.writer(
             fstream, "#define NUM_REACTIONS (NUM_GAS_REACTIONS + NUM_SURFACE_REACTIONS)"
         )
-        n_lite, __, __ = ctr.eval_lite_species(self.species_info)
-        cw.writer(
-            fstream, f"#define NUM_LITE_SPECIES {n_lite}"
-        )
-
         cw.writer(fstream)
         cw.writer(fstream, f"#define NUM_IONS {nb_ions}")
         cw.writer(fstream)
